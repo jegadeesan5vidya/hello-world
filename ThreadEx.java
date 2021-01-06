89b67ab89ca1d5572436938734267dd025ce0727
jegadeesan5vidya <40973909+jegadeesan5vidya@users.noreply.github.com>
2021-01-06 18:55:12 +0800

    Add files via upload

---
+++ ThreadEx.java
@@ -0,0 +1,6 @@
+public class ThreadEx {
+    public static void main(String[] args) {
+        Thread th = Thread.currentThread();
+        System.out.println("Current Thread details " + th);
+    }
+}
